fdca22e5427a45db542bdc42bdc7f38bef216793
nabu <abu.n@rocketfirm.tech>
2025-01-27 20:03:23 +0500

    feat: linked list

--- src/main/java/com/example/leetcodeproblems/array/LinkedListProblems.java
+++ src/main/java/com/example/leetcodeproblems/array/LinkedListProblems.java
@@ -8,6 +8,26 @@ public class LinkedListProblems {
 
     public static void main(String[] args) {
 
+        LinkedListProblems.MyLinkedList myLinkedList = new LinkedListProblems.MyLinkedList();
+        myLinkedList.addAtHead(1);
+        myLinkedList.addAtTail(3);
+        myLinkedList.addAtIndex(1, 2);    // linked list becomes 1->2->3
+        myLinkedList.get(1);              // return 2
+        myLinkedList.deleteAtIndex(1);    // now the linked list is 1->3
+        myLinkedList.get(1);
+
+//        myLinkedList.addAtHead(7);
+//        myLinkedList.addAtHead(2);
+//        myLinkedList.addAtHead(1);
+//        myLinkedList.addAtIndex(3, 0);
+//        myLinkedList.deleteAtIndex(2);
+//        myLinkedList.addAtHead(6);
+//        myLinkedList.addAtTail(4);
+//        myLinkedList.get(4);
+//        myLinkedList.addAtHead(4);
+//        myLinkedList.addAtIndex(5, 0);
+//        myLinkedList.addAtHead(6);
+
     }
 
     static boolean hasCycle(ListNode head) {
@@ -217,6 +237,90 @@ public class LinkedListProblems {
         return oddDummy;
     }
 
+    public static boolean isPalindrome(ListNode head) {
+
+        // Есть более элегантный код, посмотри в submission, запомни и напиши из памяти также
+
+        if (head == null) {
+            return false;
+        }
+
+        ListNode node = head;
+        ListNode copy = new ListNode(node.val);
+        ListNode dummy = copy;
+
+        while (node != null && node.next != null) {
+            node = node.next;
+            dummy.next = new ListNode(node.val);
+            dummy = dummy.next;
+        }
+
+
+        node = copy;
+        ListNode temp = null;
+        ListNode first = null;
+
+        while (node != null) {
+            temp = node.next;
+
+            node.next = first;
+            first = node;
+            node = temp;
+        }
+
+        ListNode main = head;
+
+        while (first != null) {
+            if (first.val != main.val) {
+                return false;
+            }
+            first = first.next;
+            main = main.next;
+        }
+
+
+        return true;
+    }
+
+    public static ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        // Решить двумя способами , есть решения в submissions, через рекурсию а второй через сорт, да сорт будет дольше но нужно попробовать решить
+        if (list1 == null) {
+            return list2;
+        }
+        if (list2 == null) {
+            return list1;
+        }
+        if (list1.val > list2.val) {
+            return mergeTwoLists(list2, list1);
+        }
+
+        ListNode head = new ListNode(0);
+        ListNode node = head;
+
+        while (list1 != null && list2 != null) {
+            if (list1.val < list2.val) {
+                node.next = list1;
+                node = node.next;
+
+                list1 = list1.next;
+            } else {
+                node.next = list2;
+                node = node.next;
+
+                list2 = list2.next;
+            }
+
+        }
+        if (list1 != null) {
+            node.next = list1;
+        }
+        if (list2 != null) {
+            node.next = list2;
+        }
+
+        return head.next;
+    }
+
     public static class ListNode {
         int val;
         ListNode next;
@@ -229,6 +333,134 @@ public class LinkedListProblems {
         ListNode(int val, ListNode next) {
             this.val = val;
             this.next = next;
+        }
+    }
+
+    public static class MyLinkedList {
+
+        Node head;
+        int size = 0;
+
+        public MyLinkedList() {
+            head = null;
+            size = 0;
+        }
+
+        public int get(int index) {
+            if (size <= index) {
+                return -1;
+            }
+            Node node = new Node(0, head);
+            for (int i = 0; i <= index; i++) {
+                node = node.next;
+            }
+            return node.val;
+        }
+
+        private void createHead(int val) {
+            head = new Node(val, null, null);
+            size = 1;
+        }
+
+        private void createTail(int val) {
+            Node node = head;
+            while (node != null && node.next != null) {
+                node = node.next;
+            }
+            Node dummy = new Node();
+            dummy.val = val;
+            dummy.prev = node;
+            node.next = dummy;
+            size++;
+        }
+
+        private void createIndex(int index, int val) {
+            Node node = new Node(0, head);
+            for (int i = 0; i <= index; i++) {
+                node = node.next;
+            }
+            Node dummy = new Node(val, node, node.prev);
+            node.prev.next = dummy;
+            node.prev = dummy;
+            size++;
+        }
+
+
+        public void addAtHead(int val) {
+            if (head == null) {
+                createHead(val);
+                return;
+            }
+            Node dummy = new Node();
+            dummy.val = val;
+            dummy.next = head;
+            head.prev = dummy;
+            head = dummy;
+            size++;
+        }
+
+        public void addAtTail(int val) {
+            if (head == null) {
+                createHead(val);
+                return;
+            }
+            createTail(val);
+        }
+
+        public void addAtIndex(int index, int val) {
+            if (index == 0) {
+                addAtHead(val);
+                return;
+            }
+            if (size == index) {
+                addAtTail(val);
+                return;
+            }
+            if (size < index) {
+                return;
+            }
+            createIndex(index, val);
+        }
+
+        public void deleteAtIndex(int index) {
+            if (size <= index) {
+                return;
+            }
+            if (index == 0) {
+                head = head.next;
+                size--;
+                return;
+            }
+            Node node = new Node(0, head);
+            for (int i = 0; i <= index; i++) {
+                node = node.next;
+            }
+            Node dummy = node;
+            dummy.prev.next = dummy.next;
+            if (dummy.next != null) {
+                dummy.next.prev = dummy.prev;
+            }
+            size--;
+        }
+
+        public static class Node {
+            public Integer val;
+            public Node next;
+            public Node prev;
+
+            public Node() {
+            }
+
+            public Node(Integer val, Node next) {
+                this.val = val;
+                this.next = next;
+            }
+
+            public Node(Integer val, Node next, Node prev) {
+                this.val = val;
+                this.next = next;
+                this.prev = prev;
+            }
         }
     }
 
