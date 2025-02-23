93b1fbaa3fcd6a98c6b69283c44c0613d7b67997
Назар Абу <nazar.abu2@berekebank.kz>
2025-02-23 23:06:20 +0500

    feat: binary search

---
+++ src/main/java/com/example/leetcodeproblems/binarysearch/FindMinimumInRotatedArray.java
@@ -0,0 +1,48 @@
+package com.example.leetcodeproblems.binarysearch;
+
+public class FindMinimumInRotatedArray {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static int findMin2(int[] nums) {
+
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left < right) {
+
+            int middle = left + (right - left) / 2;
+
+            if (nums[middle] > nums[right]) {
+                left = middle + 1;
+            } else {
+                right = middle;
+            }
+        }
+        return nums[left];
+    }
+
+    public static int findMin(int[] nums) {
+
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left < right) {
+
+            int middle = left + (right - left) / 2;
+
+            if (nums[left] > nums[middle]) {
+                right = middle;
+            } else if (nums[middle] > nums[right]) {
+                left = middle + 1;
+            } else {
+                return nums[left];
+            }
+
+        }
+        return nums[left];
+
+    }
+}
